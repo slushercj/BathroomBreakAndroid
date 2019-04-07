bee6210f2f861ad7936b4a39524d7ce5a20150de
Christopher Slusher <cslusher@illumina.com>
2019-04-07 16:00:58 -0700

    Changed the location default marker icon to bathroom break icon

--- app/src/main/java/com/slusher/android/bathroombreak/MapsActivity.java
+++ app/src/main/java/com/slusher/android/bathroombreak/MapsActivity.java
@@ -4,9 +4,9 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -14,28 +14,30 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
-    private final int MY_PERMISSION_ACCESS_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-    }
 
+    }
 
     /**
      * Manipulates the map once available.
@@ -50,39 +52,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(final GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
-            ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION  },
-                    MY_PERMISSION_ACCESS_LOCATION );
+        locateBathrooms();
+        addMarkers();
+    }
 
-            mFusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if (location != null) {
-                        LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+    private void locateBathrooms() {
 
-                        mMap.addMarker(new MarkerOptions()
-                                .position(currentLocation)
-                                .title("Marker for current location"));
-//                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 11.0f));
-                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 11.0f));
-                    }
-                }
-            });
+    }
+
+    private void addMarkers() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Toast.makeText(this, "Location permission missing", Toast.LENGTH_LONG);
+
+            return;
         }
-        else{
-            mFusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if (location != null) {
-                        LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
 
-                        mMap.addMarker(new MarkerOptions().position(currentLocation).title("Marker for current location"));
-//                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 11.0f));
-                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 11.0f));
-                    }
+        mFusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    MarkerOptions markerOptions = new MarkerOptions()
+                            .position(currentLocation)
+                            .title("Current location")
+                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_bathroom_location))
+                            .visible(true);
+
+                    mMap.addMarker(markerOptions);
+//                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 14.0f));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 14.0f));
                 }
-            });
-        }
+            }
+        });
+
     }
 }
